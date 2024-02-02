6949d85889385cd96b4e99848ae1fec031cb49cc
Denis <denis11you777602@gmail.com>
2024-02-02 17:05:30 +0300

    OpenCommit

---
+++ ProjectFromHomeTask/Program.cs
@@ -0,0 +1,81 @@
+﻿using System.Numerics;
+using System.Threading.Channels;
+
+class Work
+{
+    enum OptionsFromHMFive
+    {
+        defaultUserOptins,
+        ProgrammerTestOptions
+    }
+    static void Main()
+    {
+        HomeWork5(OptionsFromHMFive.ProgrammerTestOptions);
+    }
+    static void HomeWork1()
+    {
+        for (int i = 1; i < 99; i++)
+        {
+            if (i % 2 != 0) Console.WriteLine("число - {0} не чётное", i);
+        }
+    }
+    static void HomeWork2()
+    {
+        for(int i = 5; i > 1; i--)
+        {
+            Console.WriteLine(i);
+        }
+    }
+    static void HomeWork3()
+    {
+        Console.Write("Введите положительное число: ");
+        try
+        {
+            uint counter = 1;
+            for (; counter < Convert.ToUInt32(Console.ReadLine());)
+            {
+                counter++;
+            }
+            Console.WriteLine("ваше сумма всех чисел от 1 до {0} = {0}", counter);
+        }
+        catch
+        {
+            Console.WriteLine("Вы ввели не положительное число");
+        }
+    }
+    static void HomeWork4()
+    {
+        int secondFactor = 1;
+        for (int i = 7; i < 98;)
+        {
+            i *= secondFactor++;
+            Console.WriteLine(i);
+        }
+    }
+    static void HomeWork5(OptionsFromHMFive options)
+    {
+        int[] arrayOne = new int[5].Select(i => new Random().Next(0, 10)).ToArray(), 
+        arrayTwo = new int[5].Select(i => new Random().Next(0, 10)).ToArray();
+        // можно использовать любой из 2 массивов, у них одинаковая длинна
+        (int, int) nums = (0, 0);
+        for(int i = 0; i < arrayOne.Length; i++)
+        {
+            nums.Item1 += arrayOne[i];
+            nums.Item2 += arrayTwo[i];
+        }
+        if (options == (OptionsFromHMFive.defaultUserOptins | OptionsFromHMFive.ProgrammerTestOptions))
+        {
+            Console.WriteLine(((nums.Item1 / 5) > (nums.Item2 / 5)) ? "среднее арифметическое массива один больше" : ((nums.Item1 / 5) < (nums.Item2 / 5) ? "среднее арифметическое второго массив больше" : "среднее арифметическое массивов равны"));
+        }
+        #region Доп опции 
+        if (options == OptionsFromHMFive.ProgrammerTestOptions)
+        {
+            Console.WriteLine("Массив 1 был: ");
+            foreach (int i in arrayOne) Console.Write(i + ",");
+            Console.WriteLine();
+            Console.WriteLine("Массив 2 был: ");
+            foreach (int i in arrayTwo) Console.Write(i + ",");
+        }
+        #endregion
+    }
+}
---
+++ StarTask/Program.cs
@@ -0,0 +1,207 @@
+﻿enum VersionSort
+{
+    Reverse,
+    Normal
+}
+enum RetutnedTypeNum
+{
+    Positive, Negative
+}
+enum Actions
+{
+    Sort,
+    Return,
+    Invers
+}
+
+class Programma
+{
+    public void Start(Actions actions, int[,] matrics)
+    {
+       
+        ShowMeArray(matrics);
+        MethodAction(actions, matrics);
+    }
+    public void MethodAction(Actions actions, int[,] matrics)
+    {
+        try
+        {
+            Console.Write(actions == Actions.Return ? "Показать только отрицательные/положительные: " : (actions == Actions.Sort) ? "Отсортировать массив в обратном/нормальном порядке: " : "Инверсирую...");
+            switch (actions)
+            {
+                case Actions.Return:
+                    switch (Console.ReadLine())
+                    {
+                        case "положительные":
+                            Console.WriteLine(matrics.TakeMeAllTypesNumber(RetutnedTypeNum.Positive));
+                            break;
+                        case "отрицательные":
+                            Console.WriteLine(matrics.TakeMeAllTypesNumber(RetutnedTypeNum.Negative));
+                            break;
+                        default:
+                            Console.WriteLine(matrics.TakeMeAllTypesNumber(RetutnedTypeNum.Positive));
+                            Console.WriteLine(matrics.TakeMeAllTypesNumber(RetutnedTypeNum.Negative));
+                            break;
+                    }
+                    break;
+
+                case Actions.Sort:
+                    switch (Console.ReadLine())
+                    {
+                        case "обратном":
+                            ShowMeArray(matrics.SortNumbers(VersionSort.Reverse));
+                            break;
+                        case "нормальном":
+                            ShowMeArray(matrics.SortNumbers(VersionSort.Normal));
+                            break;
+                        default:
+                            Console.WriteLine("Не один из вариантов не был выбран");
+                            break;
+                    }
+                    break;
+                case Actions.Invers:
+                    Console.WriteLine();
+                    ShowMeArray(matrics.InversNumbers());
+                    break;
+            }
+        }
+        catch
+        {
+            Console.WriteLine("Произошла ошибка");
+        }
+    }
+    static void ShowMeArray(int[,] array)
+    {
+        for(int i = 0;i < array.GetLength(0); i++)
+        {
+            for (int j = 0;j < array.GetLength(1); j++)
+            {
+                Console.Write(array[i,j] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
+class Work
+{
+    static void Main()
+    {
+
+        try
+        {
+            Console.Write("Введите размеры матрицы: ");
+            uint sizeMatrics = Convert.ToUInt32(Console.ReadLine());
+            int[,] matrics = new int[sizeMatrics, sizeMatrics];
+            for (int i = 0; i < matrics.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrics.GetLength(1); j++)
+                {
+                    matrics[i, j] = new Random().Next(-10, 10);
+                }
+            }
+            Console.WriteLine("Выберите действия: \n1.Показать сколько положительных/отрицательных чисел\n2.Отсортировать массив\n3.Инверсировать массив.");
+            var program = new Programma();
+            switch (Convert.ToInt32(Console.ReadLine()))
+            {
+                case 1:
+                    program.Start(Actions.Return, matrics);
+
+                    break;
+                case 2:
+                    program.Start(Actions.Sort, matrics);
+                    break;
+                case 3:
+                    program.Start(Actions.Invers, matrics);
+                    break;
+                default: Console.WriteLine("Не соответствует списку"); break;
+            }
+        }
+        catch
+        {
+            Console.WriteLine("произошла ошибка");
+        }
+
+    }
+    
+}
+static class MyWorkToArray
+{
+    public static int[,] InversNumbers(this int[,] array)
+    {
+        int[] saver = new int[array.GetLength(0)];
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                saver[saver.Length - 1 - j] = array[i, j];
+            }
+            for (int k = 0; k < array.GetLength(1); k++)
+            {
+                array[i, k] = saver[k];
+            }
+        }
+        return array;
+    }
+    
+    public static string TakeMeAllTypesNumber(this int[,] array, RetutnedTypeNum type)
+    {
+        (int, int) counters = (0, 0);
+        for(int i = 0; i < array.GetLength(0); i++)
+        {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                   counters.Item1 += (Convert.ToInt32(array[i, j]) > 0) ? 1 : 0;
+                   counters.Item2 += (Convert.ToInt32(array[i, j]) < 0) ? 1 : 0;
+                 
+                }
+        }
+        return (type == RetutnedTypeNum.Positive) ? "Pos number:" + counters.Item1.ToString() : "Neg number:" + counters.Item2.ToString();
+    }
+    public static int[,] SortNumbers(this int[,] array,VersionSort sortOptions)
+    {
+        
+        int[] arraySaver = new int[array.GetLength(0)];
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                arraySaver[j] = array[i, j];
+            }
+            Sorter(ref arraySaver,sortOptions);
+            for(int k = 0;  k < array.GetLength(1); k++)
+            {
+                array[i, k] = arraySaver[k];
+            }
+        }
+        return array;
+    }
+    static void Sorter(ref int[] array, VersionSort sortOptions)
+    {
+        int NumSaver;
+        for (int i = 1;i < array.Length; i++)
+        {
+            for(int j = 0; j < array.Length - i; j++)
+            {
+                if (sortOptions == VersionSort.Normal)
+                {
+                    if (array[j] > array[j + 1])
+                    {
+                        NumSaver = array[j + 1];
+                        array[j + 1] = array[j];
+                        array[j] = NumSaver;
+                    }
+                }
+                if(sortOptions == VersionSort.Reverse)
+                {
+                    if (array[j] < array[j + 1])
+                    {
+                        NumSaver = array[j];
+                        array[j] = array[j + 1];
+                        array[j + 1] = NumSaver;
+                    }
+                }
+            }
+        }
+       
+    }
+}
